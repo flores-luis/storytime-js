6713d6365b4c0822bb76d769ca82f815c9dcccf0
flores-luis <ricardoluis6212@gmail.com>
2021-07-15 22:17:40 -0700

    testing message

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Declare variables and capture input
-
+let personName = prompt("Hello, What is your name?")
+alert("Hello")
 // Combine the input with other words to create a story
 
 // Display the story as a <p> inside the <main> element
