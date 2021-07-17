568cdc479681ce94176d97ab954d164daaa7a85d
flores-luis <ricardoluis6212@gmail.com>
2021-07-17 08:27:10 -0700

    added notes

--- js/main.js
+++ js/main.js
@@ -11,3 +11,15 @@ alert("My name is "+robotName + " "javascriptRobot)
 alert("I have learned some things about you...")
 // Display the story as a <p> inside the <main> element
 document.querySelector('main').innerHTML = 
+
+/*
+
+    Issues:
+    - <p></p> have string in this tag but what with <main></main> tag
+    - need to review document query selector rule
+    - alerts or console.log???
+
+    Need to display:
+    - message from <p></p> tag
+
+*/
