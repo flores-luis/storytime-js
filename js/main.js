a3447992a69d46255d60cd2487c70f46470fef8a
flores-luis <ricardoluis6212@gmail.com>
2021-07-16 19:09:09 -0700

    added new image

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 let personName = prompt("Hello, What is your name?")
 let favColor = prompt("What is your favorite color?")
 let favFruit = prompt("What is your favorite fruit?")
+let birthMonth = prompt("What is your birth month")
+let javascriptRobot = "The Javascript Robot"
+const robotName = "Robotron"
 // Combine the input with other words to create a story
 alert("Hello"+" "+personName+", nice to meet you...")
+alert("My name is "+robotName + " "+ javascriptRobot)
 // Display the story as a <p> inside the <main> element
