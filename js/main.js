5ee030d44aed145a5ba62cb125b7879dadef7e61
flores-luis <ricardoluis6212@gmail.com>
2021-07-16 19:02:10 -0700

    testing alerts&prompt messages

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Declare variables and capture input
 let personName = prompt("Hello, What is your name?")
-alert("Hello")
+let favColor = prompt("What is your favorite color?")
+let favFruit = prompt("What is your favorite fruit?")
 // Combine the input with other words to create a story
-
+alert("Hello"+" "+personName+", nice to meet you...")
 // Display the story as a <p> inside the <main> element
