87d403358b51729c2027f48d4273abafe8141188
flores-luis <ricardoluis6212@gmail.com>
2021-07-19 20:39:27 -0700

    fixing output

--- js/main.js
+++ js/main.js
@@ -7,10 +7,14 @@ let javascriptRobot = "The Javascript Robot"
 const robotName = "Robotron"
 // Combine the input with other words to create a story
 alert("Hello"+" "+personName+", nice to meet you...")
-alert("My name is "+robotName + " "javascriptRobot)
+alert("My name is "+robotName + " "+ javascriptRobot)
 alert("I have learned some things about you...")
+let message = "Your favorite color "+ favColor + 
+", and you favorite fruit is "+ favFruit
+", and you were born in " + birthMonth
+
 // Display the story as a <p> inside the <main> element
-document.querySelector('main').innerHTML = 
+document.querySelector('p').innerHTML = message
 // FIND THE ERROR HERE
 /*
 
@@ -21,5 +25,9 @@ document.querySelector('main').innerHTML =
 
     Need to display:
     - message from <p></p> tag
+
+
+    Solution:
+    Diplayed <p></p> in index.html page
 
 */
